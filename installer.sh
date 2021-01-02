d2b4c7453d9adff39c3ae8aa3ac16f514edf53d7
Andres Galvan <andresgalfajar@gmail.com>
2021-01-01 23:42:18 -0500

    Check if a package manager received is valid and add same title of the create-app program

--- installer.sh
+++ installer.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-echo "create-app installer"
-mkdir -p ~/.config/create-app
-wget https://raw.githubusercontent.com/afgalvan/create-app/main/create_app.sh
-chmod +x create_app.sh
-if [ -d "$1" ]; then
+is_package_manager_valid() {
+    local package_manager="$1"
+
+    if [ "$package_manager" != "npm" ] && [ "$package_manager" != "yarn" ]; then
+        echo -e "\e[31m Error on package manager name \"$package_manager\"."
+        exit 1
+    fi
+}
+
+echo -e "\e[34m create-app \e[7;34m installer \e[0m"
+# mkdir -p ~/.config/create-app
+# wget https://raw.githubusercontent.com/afgalvan/create-app/main/create_app.sh
+# chmod +x create_app.sh
+if [ -z "$1" ]; then
     pm="npm"
 else
+    is_package_manager_valid "$1"
     pm="$1"
 fi
 echo "$pm" > ~/.config/create-app/default_pm
 
-if [ -d ~/.config/create-app/create_app.sh ]; then
-    rm -f ~/.config/create-app/create_app.sh
-fi
-mv create_app.sh ~/.config/create-app/
+# if [ -d ~/.config/create-app/create_app.sh ]; then
+#     rm -f ~/.config/create-app/create_app.sh
+# fi
+# mv create_app.sh ~/.config/create-app/
