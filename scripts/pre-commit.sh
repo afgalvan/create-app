1dd057d480c59c92e3fdb7ee36513275148a5324
Andres Galvan <afgalvan@unicesar.edu.co>
2021-02-07 18:00:34 -0500

    Code-factor suggestions

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,8 +2,9 @@
 
 echo "Running pre-commit hook"
 ./test/test.sh
+exit_code=$?
 
-if [ $? -ne 0 ]; then
+if [ $exit_code -ne 0 ]; then
     echo -e "\e[31m\nTests must pass before commit!"
     exit 1
 fi
